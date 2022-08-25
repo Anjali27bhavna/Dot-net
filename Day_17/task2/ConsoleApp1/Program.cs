a718bec26a11b2261e248ba60c95a3124593f724
Anjali27bhavna <anjali.saini@bhavnacorp.com>
2022-08-25 16:52:58 +0530

    25/08/2022

---
+++ Day_17/task2/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public struct employee
+    {
+        public int id;
+        public string name;
+        public double salary;
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                employee[] emp = new employee[5];
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("Enter the employee id: ");
+                    emp[i].id = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the employee name: ");
+                    emp[i].name = Console.ReadLine();
+                    Console.WriteLine("Enter the employee salary: ");
+                    emp[i].salary = double.Parse(Console.ReadLine());
+                }
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine(emp[i].id + " " + emp[i].name + " " + emp[i].salary);
+                }
+
+
+            }
+        }
+    }
+}
