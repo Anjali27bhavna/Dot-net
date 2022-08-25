5d126359fcdca52fb7cdba2084fda9888815f458
Anjali27bhavna <anjali.saini@bhavnacorp.com>
2022-08-25 13:38:36 +0530

    25/08/2022

---
+++ Day_17/task1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //loop statement
+
+            //for (int x = 1; x <= 6; x++)
+            //{
+            //    for (int y = 1; y <= x; y++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.ReadLine();
+
+
+            //array
+            //int[] A = { 15, 20, 21, 25, 27, 30, 35 };
+            //sample arr = new sample();
+            //arr.printarray(A);
+
+
+            //constructor
+            //sample e1 = new sample(101, "Sonoo", 890000f);
+            //sample e2 = new sample(102, "Mahesh", 490000f);
+            //e1.display();
+            //e2.display();
+
+            //method overloading
+            sample ob = new sample();
+            int sum1 = ob.add(1, 2);
+            Console.WriteLine("sum of two integer value " + sum1);
+
+            float sum2 = ob.add(98f, 45f);
+            Console.WriteLine("sum of two float value " + sum2);
+
+            string sum3 = ob.add("Bhavna", "Corp");
+            Console.WriteLine("sum of two string value " + sum3);
+
+
+
+
+        }
+    }
+}
---
+++ Day_17/task1/ConsoleApp1/sample.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class sample
+    {
+        
+        //public void printarray(int[] A)
+        //{
+        //for (int i=0;i<A.Length;i++)
+        //    {
+        //    Console.Write(A[i]+ " ");
+        //   }
+        // }
+
+        //public int id;
+        //public String name;
+        //public float salary;
+        //public sample(int i, String n, float s)
+        //{
+        //    id = i;
+        //    name = n;
+        //    salary = s;
+        //}
+        //public void display()
+        //{
+        //    Console.WriteLine(id + " " + name + " " + salary);
+        //}
+
+        public int add(int a,int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+        public float add(float a, float b)
+        {
+            float sum = a + b;
+            return sum;
+        }
+        public string add(string a, string b)
+        {
+            string sum = a + b;
+            return sum;
+        }
+    }
+}
