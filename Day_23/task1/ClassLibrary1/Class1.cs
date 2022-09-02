8f6931c75b9ace71f0d7bc994c2177ffaa426f27
Anjali27bhavna <anjali.saini@bhavnacorp.com>
2022-09-02 16:47:52 +0530

    2/09/2022

---
+++ Day_23/task1/ClassLibrary1/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public class complex
+        {
+            private int x;
+            private int y;
+            public complex()
+            {
+
+            }
+            public complex(int i, int j)
+            {
+                x = i;
+                y = j;
+            }
+            public void show()
+            {
+                Console.WriteLine(x + "/" + y);
+            }
+            public static complex operator -(complex c)
+            {
+                complex temp = new complex();
+                temp.x = -c.x;
+                temp.y = -c.y;
+                return temp;
+            }
+        }
+        public class person
+        {
+            public int age;
+        }
+        public static void square(person a, person b)
+        {
+            a.age = a.age * a.age;
+            b.age = b.age * b.age;
+            Console.WriteLine(a.age + " " + b.age);
+        }
+
+
+    }
+}
